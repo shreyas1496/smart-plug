615e1d3ecf5ff994ad429cef8d30b1d82ca1dbc4
Shreyas Patankar <ssp1996@gmail.com>
2018-07-29 12:06:41 +0530

    parse body paramters

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import express from 'express';
-import morgan from 'morgan'
-const app = express()
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+const app = express();
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/slaves", (request, response) => {
-    response.json({message: "post request sent"})
-})
+  const macId = request.body.macId;
+  const ipAddr = request.body.ipAddr;
+  response.json({ message: "device registered " + macId + " " + ipAddr });
+});
 
-app.listen(3000, (err) => {
-    if(err) {
-        console.log("error")
-    }
-    else {
-        console.log("server started")
-    }
+app.listen(3000, err => {
+  if (err) {
+    console.log("error");
+  } else {
+    console.log("server started");
+  }
 });
